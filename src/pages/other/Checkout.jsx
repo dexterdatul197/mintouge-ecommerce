161c3342de01bf4d1b4cbba990df660f6696511e
guo9797 <guo@vaultik.com>
2023-08-30 17:24:19 +0100

    feat: form validation

--- src/pages/other/Checkout.jsx
+++ src/pages/other/Checkout.jsx
@@ -3,6 +3,8 @@ import { Link, Navigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import cogoToast from "cogo-toast";
 import emailjs from "@emailjs/browser";
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import { getDiscountPrice } from "../../helpers/product";
 import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
@@ -19,9 +21,11 @@ const Checkout = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [isMinting, setMinting] = useState(false);
+  const { register, handleSubmit } = useForm();  
   const hasNftList = cartItems.filter((item) =>
     item.hasOwnProperty("insuranceFee")
   );
+
   const createInsurance = async (cartItem) => {
     try {
       const dpp = String(Date.now());
@@ -178,150 +182,155 @@ const Checkout = () => {
         <div className="checkout-area pt-95 pb-100">
           <div className="container">
             {cartItems && cartItems.length >= 1 ? (
-              <div className="row">
-                <div className="col-lg-7">
-                  <div className="billing-info-wrap">
-                    <h3>Billing Details</h3>
-                    <div className="row">
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>First Name</label>
-                          <input type="text" />
+                <form onSubmit={handleSubmit(async data => {
+                  await handlePlaceOrder();
+                })}>
+                  <div className="row">
+                    <div className="col-lg-7">
+                      <div className="billing-info-wrap">
+                        <h3>Billing Details</h3>
+                        <div className="row">
+                          <div className="col-lg-6 col-md-6">
+                            <div className="billing-info mb-20">
+                              <label>First Name</label>
+                              <input type="text" name="f_name"/>
+                            </div>
+                          </div>
+                          <div className="col-lg-6 col-md-6">
+                            <div className="billing-info mb-20">
+                              <label>Last Name</label>
+                              <input type="text" name="l_name"/>
+                            </div>
+                          </div>
+                          <div className="col-lg-12">
+                            <div className="billing-info mb-20">
+                              <label>Street Address</label>
+                              <input
+                                className="billing-address"
+                                placeholder="House number and street name"
+                                type="text"
+                                name="county"
+                              />
+                              <input
+                                placeholder="Apartment, suite, unit etc."
+                                type="text"
+                                name="street"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-lg-6 col-md-6">
+                            <div className="billing-info mb-20">
+                              <label>Phone</label>
+                              <input type="text" name="phone"/>
+                            </div>
+                          </div>
+                          <div className="col-lg-6 col-md-6">
+                            <div className="billing-info mb-20">
+                              <label>Email Address</label>
+                              <input {...register("email", { required: true })} type="text" name="email"/>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Last Name</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-12">
-                        <div className="billing-info mb-20">
-                          <label>Street Address</label>
-                          <input
-                            className="billing-address"
-                            placeholder="House number and street name"
-                            type="text"
+                      <div className="additional-info-wrap">
+                        <h4>Additional information</h4>
+                        <div className="additional-info">
+                          <label>Order notes</label>
+                          <textarea
+                            placeholder="Notes about your order, e.g. special notes for delivery. "
+                            name="message"
+                            defaultValue={""}
                           />
-                          <input
-                            placeholder="Apartment, suite, unit etc."
-                            type="text"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Phone</label>
-                          <input type="text" />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="billing-info mb-20">
-                          <label>Email Address</label>
-                          <input type="text" />
                         </div>
                       </div>
                     </div>
+                  </div>
 
-                    <div className="additional-info-wrap">
-                      <h4>Additional information</h4>
-                      <div className="additional-info">
-                        <label>Order notes</label>
-                        <textarea
-                          placeholder="Notes about your order, e.g. special notes for delivery. "
-                          name="message"
-                          defaultValue={""}
-                        />
+                  <div className="col-lg-5">
+                    <div className="your-order-area">
+                      <h3>Your order</h3>
+                      <div className="your-order-wrap gray-bg-4">
+                        <div className="your-order-product-info">
+                          <div className="your-order-top">
+                            <ul>
+                              <li>Product</li>
+                              <li>Total</li>
+                            </ul>
+                          </div>
+                          <div className="your-order-middle">
+                            <ul>
+                              {cartItems.map((cartItem, key) => {
+                                const discountedPrice = getDiscountPrice(
+                                  cartItem.price,
+                                  cartItem.discount
+                                );
+                                const insuranceFee = cartItem.insuranceFee
+                                  ? cartItem.hasInsurance
+                                    ? cartItem.insuranceFee
+                                    : 0
+                                  : 0;
+                                const finalProductPrice = (
+                                  cartItem.price * currency.currencyRate +
+                                  insuranceFee
+                                ).toFixed(2);
+                                const finalDiscountedPrice = (
+                                  discountedPrice * currency.currencyRate +
+                                  insuranceFee
+                                ).toFixed(2);
+
+                                discountedPrice != null
+                                  ? (cartTotalPrice +=
+                                    finalDiscountedPrice * cartItem.quantity)
+                                  : (cartTotalPrice +=
+                                    finalProductPrice * cartItem.quantity);
+                                return (
+                                  <li key={key}>
+                                    <span className="order-middle-left">
+                                      {cartItem.name} X {cartItem.quantity}
+                                    </span>{" "}
+                                    <span className="order-price">
+                                      {discountedPrice !== null
+                                        ? currency.currencySymbol +
+                                        (
+                                          finalDiscountedPrice *
+                                          cartItem.quantity
+                                        ).toFixed(2)
+                                        : currency.currencySymbol +
+                                        (
+                                          finalProductPrice * cartItem.quantity
+                                        ).toFixed(2)}
+                                    </span>
+                                  </li>
+                                );
+                              })}
+                            </ul>
+                          </div>
+                          <div className="your-order-bottom">
+                            <ul>
+                              <li className="your-order-shipping">Shipping</li>
+                              <li>Free shipping</li>
+                            </ul>
+                          </div>
+                          <div className="your-order-total">
+                            <ul>
+                              <li className="order-total">Total</li>
+                              <li>
+                                {currency.currencySymbol +
+                                  cartTotalPrice.toFixed(2)}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="payment-method"></div>
+                      </div>
+                      <div className="place-order mt-25">
+                        <button className="btn-hover" type="submit" disabled={isMinting}>
+                          Place Order
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-
-                <div className="col-lg-5">
-                  <div className="your-order-area">
-                    <h3>Your order</h3>
-                    <div className="your-order-wrap gray-bg-4">
-                      <div className="your-order-product-info">
-                        <div className="your-order-top">
-                          <ul>
-                            <li>Product</li>
-                            <li>Total</li>
-                          </ul>
-                        </div>
-                        <div className="your-order-middle">
-                          <ul>
-                            {cartItems.map((cartItem, key) => {
-                              const discountedPrice = getDiscountPrice(
-                                cartItem.price,
-                                cartItem.discount
-                              );
-                              const insuranceFee = cartItem.insuranceFee
-                                ? cartItem.hasInsurance
-                                  ? cartItem.insuranceFee
-                                  : 0
-                                : 0;
-                              const finalProductPrice = (
-                                cartItem.price * currency.currencyRate +
-                                insuranceFee
-                              ).toFixed(2);
-                              const finalDiscountedPrice = (
-                                discountedPrice * currency.currencyRate +
-                                insuranceFee
-                              ).toFixed(2);
-
-                              discountedPrice != null
-                                ? (cartTotalPrice +=
-                                  finalDiscountedPrice * cartItem.quantity)
-                                : (cartTotalPrice +=
-                                  finalProductPrice * cartItem.quantity);
-                              return (
-                                <li key={key}>
-                                  <span className="order-middle-left">
-                                    {cartItem.name} X {cartItem.quantity}
-                                  </span>{" "}
-                                  <span className="order-price">
-                                    {discountedPrice !== null
-                                      ? currency.currencySymbol +
-                                      (
-                                        finalDiscountedPrice *
-                                        cartItem.quantity
-                                      ).toFixed(2)
-                                      : currency.currencySymbol +
-                                      (
-                                        finalProductPrice * cartItem.quantity
-                                      ).toFixed(2)}
-                                  </span>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </div>
-                        <div className="your-order-bottom">
-                          <ul>
-                            <li className="your-order-shipping">Shipping</li>
-                            <li>Free shipping</li>
-                          </ul>
-                        </div>
-                        <div className="your-order-total">
-                          <ul>
-                            <li className="order-total">Total</li>
-                            <li>
-                              {currency.currencySymbol +
-                                cartTotalPrice.toFixed(2)}
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                      <div className="payment-method"></div>
-                    </div>
-                    <div className="place-order mt-25">
-                      <button className="btn-hover" onClick={handlePlaceOrder} disabled={isMinting}>
-                        Place Order
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              </form>
             ) : (
               <div className="row">
                 <div className="col-lg-12">
