8ad6a2b392e8166f5e3b55d68ef092e22a12225c
itcvmaster <itvision0master@gmail.com>
2023-09-06 01:10:24 -0400

    fix: remove invalid import

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,7 @@ import { Provider } from 'react-redux';
 import App from "./App";
 import { store } from "./store/store";
 import PersistProvider from "./store/providers/persist-provider";
-import { setProducts } from "./store/slices/product-slice";
 import { setCategories } from "./store/slices/category-slice";
-import products from "./data/products.json";
 import categories from "./data/categories.json";
 import 'animate.css';
 import 'swiper/swiper-bundle.min.css';
@@ -14,7 +12,6 @@ import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "./assets/scss/style.scss";
 
-store.dispatch(setProducts(products));
 store.dispatch(setCategories(categories));
 
 const container = document.getElementById('root');
